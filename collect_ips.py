1ccd7c9413782fde56fe68c38d1a299e6a64dc5a
linzeli586 <152822709+linzeli586@users.noreply.github.com>
2025-04-29 21:34:15 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -5,8 +5,7 @@ import os
 
 # 目标URL列表
 urls = ['https://monitor.gacjie.cn/page/cloudflare/ipv4.html', 
-        'https://ip.164746.xyz',
-        'https://github.com/ymyuuu/IPDB/blob/main/BestCF/bestcfv4.txt'
+        'https://ip.164746.xyz'
         ]
 
 # 正则表达式用于匹配IP地址
@@ -29,8 +28,6 @@ with open('ip.txt', 'w') as file:
         if url == 'https://monitor.gacjie.cn/page/cloudflare/ipv4.html':
             elements = soup.find_all('tr')
         elif url == 'https://ip.164746.xyz':
-            elements = soup.find_all('tr')
-        elif url == 'https://github.com/ymyuuu/IPDB/blob/main/BestCF/bestcfv4.txt':
             elements = soup.find_all('tr')
         else:
             elements = soup.find_all('li')
